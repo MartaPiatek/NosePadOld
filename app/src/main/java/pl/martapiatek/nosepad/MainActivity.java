4259b6345c365409b2a194600b98d7514ea99cda
MartaPiatek <martusia.piatek@gmail.com>
2017-07-19 23:06:17 +0200

    Ustawienie wyświetlania ekranu powitalnego

--- app/src/main/java/pl/martapiatek/nosepad/MainActivity.java
+++ app/src/main/java/pl/martapiatek/nosepad/MainActivity.java
@@ -1,13 +1,62 @@
 package pl.martapiatek.nosepad;
 
+import android.app.Dialog;
+import android.os.AsyncTask;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements Runnable{
+
+
+    private Handler handler;
+    private Dialog splashDialog;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        showSplashScreen();
+        handler = new Handler();
+        AsyncTask.execute(this);
+
+
+
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        dismissSplashScreen();
+    }
+
+    private void showSplashScreen() {
+        splashDialog = new Dialog(this, R.style.splash_screen);
+        splashDialog.setContentView(R.layout.activity_splash);
+        splashDialog.setCancelable(false);
+        splashDialog.show();
+    }
+
+    private void dismissSplashScreen() {
+        if (splashDialog != null) {
+            splashDialog.dismiss();
+            splashDialog = null;
+        }
+    }
+    @Override
+    public void run() {
+            handler.postDelayed(new Runnable() {
+                @Override
+                public void run() {
+
+                    dismissSplashScreen();
+
+                }
+            }, 5000
+                );
     }
 }
