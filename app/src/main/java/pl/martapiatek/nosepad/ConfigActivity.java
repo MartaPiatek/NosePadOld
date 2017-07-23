44151223b2137469675c57c2eee13abb27b9d748
MartaPiatek <martusia.piatek@gmail.com>
2017-07-23 15:47:53 +0200

    Dodawanie przykładowych recenzji w menu konfiguracyjnym

--- app/src/main/java/pl/martapiatek/nosepad/ConfigActivity.java
+++ app/src/main/java/pl/martapiatek/nosepad/ConfigActivity.java
@@ -9,7 +9,7 @@ import android.widget.Toast;
 public class ConfigActivity extends AppCompatActivity {
 
     private NosePadDbAdapter mDbAdapter;
-    private Button btnRemove;
+    private Button btnRemove, btnAdd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,5 +31,33 @@ public class ConfigActivity extends AppCompatActivity {
                 Toast.makeText(getApplicationContext(), "Usunięto", Toast.LENGTH_SHORT).show();
             }
         });
+
+
+        btnAdd = (Button)findViewById(R.id.btnAdd);
+
+        btnAdd.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+
+                mDbAdapter.open();
+                insertSomeReviews();
+                mDbAdapter.close();
+
+                Toast.makeText(getApplicationContext(), "Dodano", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+    }
+
+    private void insertSomeReviews() {
+        mDbAdapter.createReview("Prada", "Pradowska", "Cytryna", "Spoko",3);
+        mDbAdapter.createReview("Armani", "Pierdani", "Limonka", "OK",10);
+        mDbAdapter.createReview("Armani", "Siedzani", "Cytryna", "Spoko",1);
+        mDbAdapter.createReview("Chanel", "No 5", "cytruski", "Spoko",8);
+        mDbAdapter.createReview("Penhaligon's", "Vaara", "smrodki mojej Klejnotki", "OK",5);
+        mDbAdapter.createReview("Armani", "Mani", "słodziaki pierdziaki", "Super",9);
+
     }
 }
